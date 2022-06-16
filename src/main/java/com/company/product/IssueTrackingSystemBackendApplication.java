45c765610b640927fdc12f3334dd52e6e1f04572
Afrar Malakooth <mmafrar@gmail.com>
2022-06-16 23:01:57 +0530

    Setting up the development environment

---
+++ src/main/java/com/company/product/IssueTrackingSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.company.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackingSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackingSystemBackendApplication.class, args);
+	}
+
+}
